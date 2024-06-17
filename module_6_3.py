7f6cb86b83acf6645ec438db85268e9803af4db3
MisterCbing <da@susanin.news>
2024-06-17 19:30:16 +0400

    v 1.1 - исправлен метод get_horse_powers.

--- module_6_3.py
+++ module_6_3.py
@@ -3,16 +3,18 @@ class Vehicle:
 
 class Car:
     price = 1000000
-    def horse_powers(self):
-        return self.horse_powers()
+    horse_powers = 200
+    def get_horse_powers(self):
+        return self.horse_powers
 
 class Nissan(Car, Vehicle):
     vehicle_type = 'bullet'
     price = 1500000
 
-    def horse_powers(self):
-        return self.horse_powers()
+    def get_horse_powers(self):
+        print(self.horse_powers)
 
 nissan_skyline = Nissan()
 print(nissan_skyline.vehicle_type)
 print(nissan_skyline.price)
+nissan_skyline.get_horse_powers()
