540c171f75b4c60bec876e9218559b406fa06428
MisterCbing <da@susanin.news>
2024-06-18 23:16:40 +0400

    v 0.2 работают только get_color и get_sides

--- module6hard.py
+++ module6hard.py
@@ -1,10 +1,15 @@
+import math
+
 class Figure():
     sides_count = 0
+    def __is_valid_sides(self, __sides):
+        if len(list(__sides)) == self.sides_count:
+            return True
 
-    def __init__(self, __color, __sides, filled = False):
+    def __init__(self, __color, *__sides, filled = False):
         self.__color = __color
-        self.__sides = __sides
         self.filled = filled
+        self.__sides = list(__sides)
 
     def get_color(self):
         return self.__color
@@ -17,14 +22,28 @@ class Figure():
         if self.__is_valid_color(__color):
             self.__color  = __color
 
+    def set_sides(self, *__sides):
+        if self.__is_valid_sides(__sides):
+            self.__sides = __sides
+
+    def get_sides(self):
+        return self.__sides
+
+    def len(self):
+        return sum(self.__sides)
+
 class Circle(Figure):
     sides_count = 1
+    def len(self):
+        return self._Figure__sides[0] * 2 * math.pi
 
 class Triangle(Figure):
     sides_count = 3
 
 class Cube(Figure):
     sides_count = 12
+    def get_sides(self):
+        return self._Figure__sides * self.sides_count
 
 circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
 cube1 = Cube((222, 35, 130), 6)
@@ -33,4 +52,4 @@ cube1 = Cube((222, 35, 130), 6)
 circle1.set_color(55, 66, 77) # Изменится
 cube1.set_color(300, 70, 15) # Не изменится
 print(circle1.get_color())
-print(cube1.get_color())
+print(cube1.get_sides())
